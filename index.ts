d2a77ac853f77c98bc6040a08219ef0a049d05ee
Tahlil <bsse0803@iit.du.ac.bd>
2023-04-05 00:28:20 +0600

    Check if to address make any difference for gas cost

--- index.ts
+++ index.ts
@@ -16,7 +16,7 @@ const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 console.log(signer.address);
 
 
-async function getGasCost(address: string, amount: number) {
+async function getGasCost(address: string, amount: number, toAddress: string) {
     const usdcAmount = amount;
 
     const balance = await contract.balanceOf(address);
@@ -25,7 +25,7 @@ async function getGasCost(address: string, amount: number) {
     console.log("Gas cost:");
     
     if(usdcAmount <= balance){
-        gasCost = await (contract.estimateGas.transfer("0x922F709CE161b2B80fc626f65bAA52112752489A", usdcAmount, {from: address}));    
+        gasCost = await (contract.estimateGas.transfer(toAddress, usdcAmount, {from: address}));    
         gasCost = parseInt(gasCost.toString());
         return gasCost;
     }
@@ -37,11 +37,19 @@ async function getGasCost(address: string, amount: number) {
 const addressWithSufficientBalance1 = signer.address;
 const addressWithSufficientBalance2 = "0x99767C57701afac09D1D2a1Cb5d3BF30f63e3964";
 const addressWithInsufficientBalance = "0xB59313B00116F7e729dBCB8CF2b868bcD7629533";
+const toAddress1 = "0x1147d6F8049c16f80212290B672fBe89E9A91D29"
+const toAddress2 = "0x2cd02ad9B1304fcc50FEbE6A41e663B9Ef69666a"
 async function main() {
-    console.log(await getGasCost(addressWithSufficientBalance1, 1000));
-    console.log(await getGasCost(addressWithSufficientBalance2, 1000));
-    console.log(await getGasCost(addressWithInsufficientBalance, 1000)); 
+    console.log(await getGasCost(addressWithSufficientBalance1, 1000, toAddress1));
+    console.log(await getGasCost(addressWithSufficientBalance2, 1000, toAddress1));
+    console.log(await getGasCost(addressWithInsufficientBalance, 1000, toAddress1)); 
 }
-main();
 
+// main();
+async function toAddressExperiment(){
+    console.log(await getGasCost(addressWithSufficientBalance1, 1000, toAddress1));
+    console.log(await getGasCost(addressWithSufficientBalance1, 1000, toAddress2));
+
+}
+toAddressExperiment();
 
