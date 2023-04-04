9b31c5c094d9a0ec03cb3a9ff8bbb1a178c43231
Tahlil <bsse0803@iit.du.ac.bd>
2023-04-04 20:07:51 +0600

    Add signer and update abi

--- index.ts
+++ index.ts
@@ -6,16 +6,21 @@ dotenv.config();
 const rpcURL = process.env.ETH_GOERLI_API_URL;
 
 // Create a provider instance to connect to the Ethereum network
-const provider = new ethers.JsonRpcProvider(rpcURL);
+const provider = new ethers.providers.JsonRpcProvider(rpcURL);
 
 // Create an instance of the Contract class
 const contractAddress = '0x2f3A40A3db8a7e3D09B0adfEfbCe4f6F81927557';
+const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 const abi = myContractAbi;
 const contract = new ethers.Contract(contractAddress, abi, provider);
 console.log(contract);
 
 
 async function main() {
-    // const gasCost = await contract.estimateGas.transfer()    
+    const amountInEther = "0.1";
+    const amount = ethers.utils.parseEther(amountInEther);
+    const gasCost = await contract.estimateGas.transfer("0x922F709CE161b2B80fc626f65bAA52112752489A", amount, {from: signer.address, gasPrice: provider.getGasPrice()});    
+    console.log(gasCost);
+    
 }
 main();
