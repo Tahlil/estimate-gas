7c78550bad2647590f4ae51cfe0295fcff87e05e
Tahlil <bsse0803@iit.du.ac.bd>
2023-04-04 18:13:11 +0600

    Create usdc contract

--- index.ts
+++ index.ts
@@ -1,4 +1,14 @@
-import dotenv from 'dotenv';
+import * as dotenv from "dotenv";
+import { ethers } from 'ethers';
+import * as myContractAbi from './abi/abi.json';
 
 dotenv.config();
-const provider = process.env.ETH_GOERLI_API_URL;
+const rpcURL = process.env.ETH_GOERLI_API_URL;
+
+// Create a provider instance to connect to the Ethereum network
+const provider = new ethers.JsonRpcProvider(rpcURL);
+
+// Create an instance of the Contract class
+const contractAddress = '0x2f3A40A3db8a7e3D09B0adfEfbCe4f6F81927557';
+const abi = myContractAbi;
+const contract = new ethers.Contract(contractAddress, abi, provider);
